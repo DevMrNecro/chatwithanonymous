4bfbf4b363cfc6b303bfdba92835f8d6a74849c6
DevMrNecro <ojasbarik.dev@gmail.com>
2021-06-26 17:05:10 +0530

    first commit

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from typing import cast
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ucb82xl3nrcu@fo1bm^ad**skc5j!hi_0oevc)3_-+(u$3$ef2'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['f12885d5996f.ngrok.io','127.0.0.1']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS')
 
 
 # Application definition
